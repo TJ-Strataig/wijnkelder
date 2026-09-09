1ee25cd5403dded63d6c6ece6a7552ed84d16f81
Tije Rotman <tirotman@microsoft.com>
2026-09-09 08:12:02 +0200

    config.js hersteld

--- web/config.js
+++ web/config.js
@@ -1,5 +1,5 @@
 // Instellingen van de webapp. Pas API_BASE aan naar het adres van je Cloudflare Worker.
 // Voorbeeld: "https://wijnkelder-api.jouwnaam.workers.dev"
-export const API_BASE = 'https://wijnkelder-api.JOUW-SUBDOMEIN.workers.dev';
+export const API_BASE = 'https://wijnkelder-api.rotman-tije.workers.dev';
 export const APP_NAME = 'Wijnkelder van Angela & Tije';
 export const CURRENCY = 'EUR';
